92da7bbe861f429eca78f4b6fceaac153b392071
kim-elijah-sol <kimelijahsol@gmail.com>
2025-10-02 13:49:34 +0000

    feat(ui): `utils` > useControllableState

---
+++ packages/ui/src/_utils/hooks/useControllableState.hook.ts
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+export const useControllableState = <T>(
+  controlledValue?: T,
+  defaultValue?: T,
+  onChange?: (value: T) => void
+) => {
+  const [uncontrolledValue, setUncontrolledValue] = useState(defaultValue);
+  const isControlled = controlledValue !== undefined;
+  const value = isControlled ? controlledValue : uncontrolledValue;
+
+  const setValue = (newValue: T) => {
+    if (!isControlled) {
+      setUncontrolledValue(newValue);
+    }
+    onChange?.(newValue);
+  };
+
+  return [value, setValue] as const;
+};
