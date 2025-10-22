eff0c5942821cdf107347cd4959734c7dd28291b
kim-elijah-sol <kimelijahsol@gmail.com>
2025-10-22 13:02:04 +0000

    feat(ui): `utils` > Prefix type

--- packages/ui/src/_utils/types/index.ts
+++ packages/ui/src/_utils/types/index.ts
@@ -2,5 +2,6 @@ export * from './ArrayElementOf.types';
 export * from './AsProps.types';
 export * from './CssMap.types';
 export * from './PickRequired.types';
+export * from './Prefix.types';
 export * from './StringElementType.types';
 export * from './WithoutRef.types';
